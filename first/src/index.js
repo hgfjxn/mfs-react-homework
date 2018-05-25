0da7bbe759eac30e09c138a421036191da30a38d
Guangfu He <hgfdodo@gmail.com>
2018-05-26 02:35:26 +0800

    commit day2 homework

--- first/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import HelloComponent from './App';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<HelloComponent />, document.getElementById('root'));
-registerServiceWorker();
